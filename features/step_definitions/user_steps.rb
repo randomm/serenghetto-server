18747e2ca47ca5d3aa3b79e5889dc6b1557813a5
Jani Turunen <turunen.jani@gmail.com>
2011-09-30 16:56:05 +0300

    all devise tests pass

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -9,10 +9,16 @@ Given /^I am a user named "([^"]*)" with an email "([^"]*)" and password "([^"]*
             :password_confirmation => password).save!
 end
 
+Given /^I am admin$/ do
+  Factory(:admin)
+end
+
+# User == nil would probably be better?
 Then /^I should be already signed in$/ do
   And %{I should see "Logout"}
 end
 
+# Separate "Signed up as" (Just insert the user to the variables) and "I login through the login screen" (Fill in the login form)
 Given /^I am signed up as "(.*)\/(.*)"$/ do |email, password|
   Given %{I am not logged in}
   When %{I go to the sign up page}
@@ -24,18 +30,22 @@ Given /^I am signed up as "(.*)\/(.*)"$/ do |email, password|
   And %{I am logout}
 end
 
+# /sign_out logs out the user instantly?
 Then /^I sign out$/ do
   visit('/users/sign_out')
 end
 
+# "I am not logged in" sounds better
 Given /^I am logout$/ do
   Given %{I sign out}
 end
 
+# User == nil ? ok : @user = nil
 Given /^I am not logged in$/ do
   Given %{I sign out}
 end
 
+# Duplicate.. same thing as above.
 When /^I sign in as "(.*)\/(.*)"$/ do |email, password|
   Given %{I am not logged in}
   When %{I go to the sign in page}
@@ -44,10 +54,12 @@ When /^I sign in as "(.*)\/(.*)"$/ do |email, password|
   And %{I press "Sign in"}
 end
 
+# Check if signed in should check if the user is signed in, not if there's some text on screen (That can be tested with "I should see..")
 Then /^I should be signed in$/ do
   Then %{I should see "Signed in successfully."}
 end
 
+# ?
 When /^I return next time$/ do
   And %{I go to the home page}
 end
@@ -56,4 +68,21 @@ Then /^I should be signed out$/ do
   And %{I should see "Sign up"}
   And %{I should see "Login"}
   And %{I should not see "Logout"}
+end
+
+When /^I change old password "([^"]*)" to "([^"]*)"$/ do |old_password, new_password|
+  And %{I fill in "Password" with "#{new_password}"}
+  And %{I fill in "Password confirmation" with "#{new_password}"}
+  And %{I fill in "Current password" with "#{old_password}"}
+  And %{I press "Update"}
+end
+
+Then /^new password should be "([^"]*)"$/ do |new_password|
+  # test this via changing user name with new password
+  When %{I follow "Edit account"}
+  And %{I fill in "Name" with "baz"}
+  And %{I fill in "Current password" with "#{new_password}"}
+  And %{I press "Update"}
+  And %{I follow "Edit account"}
+  Then %{the "Name" field should equal "baz"}
 end
--- features/users/user_edit.feature
+++ features/users/user_edit.feature
@@ -1,15 +1,31 @@
 Feature: Edit User
   As a registered user of the website
   I want to edit my user profile
-  so I can change my username
+  so I can change all my user details
 
-    Scenario: I sign in and edit my account
+    Background: I sign in
       Given I am a user named "foo" with an email "user@test.com" and password "please"
-      When I sign in as "user@test.com/please"
+      And I sign in as "user@test.com/please"
       Then I should be signed in
+
+    Scenario: I edit my user name
       When I follow "Edit account"
       And I fill in "Name" with "baz"
       And I fill in "Current password" with "please"
       And I press "Update"
-      And I go to the homepage
-      Then I should see "User: baz"
+      And I follow "Edit account"
+      Then the "Name" field should equal "baz"
+
+    Scenario: I edit my email address
+      When I follow "Edit account"
+      And I fill in "Email" with "asdf@asdf.com"
+      And I fill in "Current password" with "please"
+      And I press "Update"
+      And I follow "Edit account"
+      Then the "Email" field should equal "asdf@asdf.com"
+
+    Scenario: I change my password
+      When I follow "Edit account"
+      And I change old password "please" to "pleaseplease"
+      Then I should not see "Password doesn't match confirmation"
+      And new password should be "pleaseplease"
