877fd677fc43376fb8cedd2146c22fb8856f9a0c
Jani Turunen <turunen.jani@gmail.com>
2011-09-30 13:01:58 +0300

    first commit of "code name" project's server component

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CodeName::Application.config.secret_token = '318a40fe05db002d75af137ac4c1b4ad87c374a42e175728a24479e4b7d01a1e5d88e32151d368863c551b5a2609ff08fb7658bed94cbf41a93471affae1b8ea'
