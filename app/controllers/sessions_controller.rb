79e886de23906158f36a0cf7cf55a31f0a5bd19b
Jani Turunen <turunen.jani@gmail.com>
2011-10-19 14:49:20 +0300

    added empty body to 403 response

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -28,7 +28,7 @@ class SessionsController < Devise::SessionsController
   end
 
   def failure
-    return render :status => 403, :json => {:message => "Log in failed! Wrong email address or password, perhaps?" }
+    return render :status => 403, :json => {:message => "Log in failed! Wrong email address or password, perhaps?", :body => {} }
   end
 
 end
