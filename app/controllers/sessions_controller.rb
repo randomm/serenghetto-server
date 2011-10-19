139302053122c10d363f1bd028bc2178ccb1f5d0
Jani Turunen <turunen.jani@gmail.com>
2011-10-19 14:09:39 +0300

    changed schema

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -23,7 +23,7 @@ class SessionsController < Devise::SessionsController
     if !current_user.authentication_token
       current_user.reset_authentication_token!
     end
-    return render :status => 201, :json => {:success => true, :user => { :email => current_user.email, :id => current_user.id }, :auth_token => current_user.authentication_token }
+    return render :status => 201, :json => {:msg => "Logged in successfully!", :body => { :user => { :email => current_user.email, :id => current_user.id }, :token => current_user.authentication_token }}
 #    return render :status => 200, :json => {:success => true, :redirect => stored_location_for(scope) || after_sign_in_path_for(resource)}
   end
 
