ea36ba6087fb486fad80111f96d803a83bd40c89
Jani Turunen <turunen.jani@gmail.com>
2011-10-24 15:00:05 +0300

    Adding barcode tests pass.

--- app/controllers/barcode_controller.rb
+++ app/controllers/barcode_controller.rb
@@ -23,13 +23,16 @@ class BarcodeController < ApplicationController
       message = "Barcode saved. "
       
       # check if we got location data and save that as well
+      breakpoint
+      0
+
       if params[:location] && params[:location].has_key?(:latitude) && params[:location].has_key?(:longitude) && params[:location].has_key?(:timestamp) && params[:location].has_key?(:accuracy)
         @barcode_location = BarcodeLocation.create({
           :barcode_id => @barcode.id,
           :user_id => current_user.id,
-          :geom => Point.from_x_y(params[:location][:longitude], params[:location][:latitude], 4326),
+          :geom => Point.from_x_y(params[:location][:longitude].to_f, params[:location][:latitude].to_f, 4326),
           :device_timestamp => Time.at(params[:location][:timestamp].to_i),
-          :accuracy => params[:location][:accuracy].to_f
+          :accuracy => params[:location][:accuracy]
         })
         message += "Location information saved."
       end
