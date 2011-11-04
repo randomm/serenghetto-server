7c4c26c7c0eb09cb1a9acbc023a4ebd306d9bf4f
Jani Turunen <turunen.jani@gmail.com>
2011-11-04 13:48:42 +0200

    connected user_location to user model through acts_as_api templates

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ActiveRecord::Base
 
   # set up relationships to other models
   has_many :barcodes
+  has_one :location, :class_name => 'UserLocation', :order => 'id DESC'
 
   # set to act as API for JSON responses
   acts_as_api
@@ -16,12 +17,14 @@ class User < ActiveRecord::Base
     t.add :email
     t.add :id
     t.add :name
-    t.add :authentication_token
+    t.add :authentication_token, :as => :token
+    t.add :location, :template => :private
   end
   
   api_accessible :public do | t |
     t.add :email
     t.add :name
+    t.add :location, :template => :public
   end
   
 end
--- app/models/user_location.rb
+++ app/models/user_location.rb
@@ -1,3 +1,17 @@
 class UserLocation < ActiveRecord::Base
   belongs_to :user, :foreign_key => :user_id
+
+  # set to act as API for JSON responses
+  acts_as_api
+  
+  api_accessible :private do | t |
+    t.add :id
+    t.add :geom
+    t.add :description
+  end
+  
+  api_accessible :public do | t |
+    t.add :description
+  end
+
 end
